001a4bb7f2beb433d826a1f3f789c14df86c39b6
bradyw97 <bradyw97@vt.edu>
2018-10-30 20:39:15 -0400

    forgot to submit

--- L20/opi.c
+++ L20/opi.c
@@ -1,29 +1,53 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
+
+
 
 int main(int argc, char **argv){
 
-  long long int Ninside = 0; // number of random points inside 1/4 circle
-  long long int Ntests = 1000000000;
-  long long n;
-  
-  double estpi = 0;
 
-  srand48(12345);
+  // takes command line arg for how many threads akah wants
+  int Nthreads = atoi(argv[1]);
+
+  // does some akah
+  struct drand48_data buff;
+   
+
+
+  long long int Ninside; // number of random points inside 1/4 circle
+ 
+  long long int Ntests = 1000;
+  long long n;
+  double x, y, t_start, t_end, estpi;
+
+    // declares no. of threads
+  omp_set_num_threads(Nthreads);
+  
+  t_start = omp_get_wtime();
+  Ninside = 0;
+
+  srand48_r(omp_get_thread_num(), &buff);
+
+#pragma omp for reduction(+:Ninside)
   
   for(n=0;n<Ntests;++n){
-    double x = drand48();
-    double y = drand48();
+    drand48_r(&buff, &x);
+    drand48_r(&buff, &y);
     
     if(x*x+y*y<1){
       ++Ninside;
     }
   }
 
+  
   estpi = 4.*(Ninside/(double)Ntests);
 
-  printf("estPi = %lf\n", estpi);
+  t_end = omp_get_wtime();
 
+  printf("estPi = %lf\n", estpi);
+  printf("time elapsed = %f\n", t_end-t_start);
+  
   return 0;
 }
